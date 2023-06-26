2a2d58b7af98b505ab97437c92febd7c855368c3
mikearche <mikearche@gmail.com>
2023-06-26 16:08:57 -0700

    Implement initial PoC

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,53 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import * as PIXI from "pixi.js";
+import smile from "/smile.svg";
+import smileInner from "/smile_inner.svg";
+import shiny from "/texture.jpg";
+import shinyGold from "/gold_foil.jpg";
+import vertShad from "/vertShad.txt";
+import fragShad from "/fragShad.txt";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = new PIXI.Application<HTMLCanvasElement>({
+  background: "#1099bb",
+  width: 800,
+  height: 800,
+});
+PIXI.settings.PREFER_ENV = PIXI.ENV.WEBGL2;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const fragShadLod = await PIXI.Assets.load<string>(fragShad);
+const vertShadLod = await PIXI.Assets.load<string>(vertShad);
+
+// generate random seed
+const seed = Math.max(1, Math.random() * 360);
+
+// Adding the application's view to the DOM
+document.body.appendChild(app.view);
+
+const smileSprite = PIXI.Sprite.from(smile);
+const backText = PIXI.Sprite.from(shiny);
+const backText2 = PIXI.Sprite.from(shinyGold);
+const smileInnerSprite = PIXI.Sprite.from(smileInner);
+backText.width = 600;
+backText.height = 600;
+backText2.width = 600;
+backText2.height = 600;
+smileSprite.width = 600;
+smileSprite.height = 600;
+smileInnerSprite.width = 600;
+smileInnerSprite.height = 600;
+
+backText.anchor.set(0.5);
+backText.position.set(300, 300);
+backText.angle = seed;
+
+backText2.anchor.set(0.5);
+backText2.position.set(300, 300);
+backText2.angle = seed;
+
+smileSprite.blendMode = PIXI.BLEND_MODES.DST_ATOP;
+backText.mask = smileSprite;
+backText2.mask = smileInnerSprite;
+app.stage.addChild(backText2);
+app.stage.addChild(smileInnerSprite);
+app.stage.addChild(backText);
+app.stage.addChild(smileSprite);
